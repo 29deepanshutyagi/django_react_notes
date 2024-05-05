94709262afef12be9a9deb1622092243932eef5d
Deepanshu Tyagi <29deepanshutyagi.com>
2024-05-05 23:44:02 +0530

    intial work

---
+++ backend/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    permission_classes=[AllowAny]
+    
+    
---
+++ backend/backend/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from api.views import CreateUserView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import  TokenObtainPairView, TokenRefreshView
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),  # Use MyTokenObtainPairView
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth/", include("rest_framework.urls")),
+]
